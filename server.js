d73b88dddd3bbf3c3ba557272e20cf08a6b44200
ryaannchristiaann0702 <ryaannchristiaann@gmail.com>
2017-06-16 17:37:17 +0800

    fsgfd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'views/app.html'));
+  res.sendFile(path.join(__dirname, 'views/ASSIGNMENT.html'));
 });
 
 //express server listen
